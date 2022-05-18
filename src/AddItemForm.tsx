43fa0a4df0d27aee065b9a979bfb2cf049ba67c3
Ilya Pasternak <pasterzoom@gmail.com>
2022-05-18 19:34:42 +0300

    first

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,52 @@
+import { IconButton, TextField } from '@mui/material';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import AddIcon from '@mui/icons-material/Add';
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+export function AddItemForm(props: AddItemFormPropsType) {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.key === 'Enter') {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField
+            value={title}
+            onChange={onChangeHandler}
+            onKeyPress={onKeyPressHandler}
+            id={error? "outlined-error-helper-text": "outlined-basic"}
+            label="Enter your text"
+            variant="outlined"
+            error={!!error}
+            helperText={error && "Incorrect entry."}
+            size={'small'}
+        />
+        <IconButton onClick={addItem} aria-label="delete" size="medium">
+            <AddIcon />
+        </IconButton>
+
+        {/*{error && <div className="error-message">{error}</div>}*/}
+    </div>
+}
---
+++ src/EditableSpan.tsx
@@ -0,0 +1,37 @@
+import React, {ChangeEvent, useState} from 'react';
+import {TextField} from '@mui/material';
+
+type EditableSpanPropsType = {
+    value: string
+    onChange: (newValue: string) => void
+}
+
+export function EditableSpan(props: EditableSpanPropsType) {
+    let [editMode, setEditMode] = useState(false);
+    let [title, setTitle] = useState(props.value);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+        setTitle(props.value);
+    }
+    const activateViewMode = () => {
+        setEditMode(false);
+        props.onChange(title);
+    }
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    return editMode
+        ?
+        //<input value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
+        <TextField
+            variant="standard"
+            onBlur={activateViewMode}
+            autoFocus
+            value={title}
+            onChange={changeTitle}
+            size={'small'}
+        />
+        : <span onDoubleClick={activateEditMode}>{props.value}</span>
+}
