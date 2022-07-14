aed0c909d115deabfb5d2e29e0684160af47ec2d
Ilya Pasternak <pasterzoom@gmail.com>
2022-07-14 22:20:44 +0300

    15 - Четверг 3. Errors, loadings

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,6 +1,7 @@
 import {Dispatch} from 'redux'
 import {authAPI} from '../api/todolists-api'
 import {setIsLoggedInAC} from '../features/Login/auth-reducer'
+import {handleServerAppError, handleServerNetworkError} from '../utils/error-utils'
 
 const initialState: InitialStateType = {
     status: 'idle',
@@ -39,9 +40,14 @@ export const initializeAppTC = () => (dispatch: Dispatch) => {
     authAPI.me().then(res => {
         if (res.data.resultCode === 0) {
             dispatch(setIsLoggedInAC(true))
+            dispatch(setAppStatusAC('succeeded'))
         } else {
+            handleServerAppError(res.data, dispatch)
         }
     })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
         .finally(() => {
             dispatch(setIsInitializedAC(true))
         })
